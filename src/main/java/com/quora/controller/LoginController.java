1752116febd278575d289f8008a990f0b2819655
Sunny_sunny321 <lyzhu0318@163.com>
2018-05-02 17:04:12 +0800

    a little change

--- src/main/java/com/quora/controller/LoginController.java
+++ src/main/java/com/quora/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -44,16 +45,14 @@ public class LoginController {
                       @RequestParam(value = "next", required = false) String next,
                       HttpServletResponse response) {
 
-
         try {
             Map<String, String> res = userService.register(name, password);
-
             if (res.containsKey("ticket")) {
                 //包含ticket, 写入cookie
                 Cookie cookie = new Cookie("ticket", res.get("ticket"));
                 cookie.setPath("/"); //可在同一应用服务器内共享方法
                 response.addCookie(cookie);
-                if (next != null)
+                if (!StringUtils.isEmpty(next))
                     return "redirect:" + next;
                 else
                     return "redirect:/";
@@ -67,14 +66,6 @@ public class LoginController {
             logger.info(e.getMessage());
             return "login";
         }
-    }
-
-    //注册页面: 访问/reglogin, 然后显示login.ftl页面
-    //在login.ftl中: 登陆->/login/, 注册->/reg/
-    @RequestMapping(path = "/reglogin", method = RequestMethod.GET)
-    public String reg(Model model, @RequestParam(value = "next", required = false) String next) {
-        model.addAttribute("next", next);
-        return "login";
     }
 
     /**
@@ -100,12 +91,12 @@ public class LoginController {
                 Cookie cookie = new Cookie("ticket", res.get("ticket"));
                 cookie.setPath("/"); //可在同一应用服务其内共享方法
                 response.addCookie(cookie);
-                if (next == null)
+                if (StringUtils.isEmpty(next))
                     return "redirect:/";
                 else
                     return "redirect:" + next;
             } else {
-                //注册发生异常
+                //登录发生异常
                 model.addAttribute("msg", res.get("msg"));
                 return "login";
             }
@@ -115,6 +106,16 @@ public class LoginController {
         }
 
     }
+
+    //注册页面: 访问/reglogin, 然后显示login.ftl页面
+    //在login.ftl中: 登陆->/login/, 注册->/reg/
+    @RequestMapping(path = "/reglogin", method = RequestMethod.GET)
+    public String reglogin(Model model, @RequestParam(value = "next", required = false) String next) {
+        model.addAttribute("next", next);
+        return "login";
+    }
+
+
 
     @RequestMapping(path = "/logout", method = RequestMethod.GET)
     public String logout(@CookieValue("ticket") String ticket) {
