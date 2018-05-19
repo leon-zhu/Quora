21755f51b1b77a3caaed05fd3034a59d22adee10
Sunny_sunny321 <lyzhu0318@163.com>
2018-05-19 21:47:19 +0800

    improve the Trie algorithm

--- src/main/java/com/quora/service/SensitiveWordsService.java
+++ src/main/java/com/quora/service/SensitiveWordsService.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 敏感词过滤
+ * 敏感词过滤 - Trie树/单词查找树/字典树
  *
  * @author: leon
  * @date: 2018/5/4 10:38
@@ -47,7 +47,7 @@ public class SensitiveWordsService implements InitializingBean {
             return isEnd;
         }
 
-        void setKeyWotdEnd(boolean isEnd) {
+        void setKeyWordEnd(boolean isEnd) {
             this.isEnd = isEnd;
         }
 
@@ -79,6 +79,8 @@ public class SensitiveWordsService implements InitializingBean {
 
     }
 
+    private TrieNode root = new TrieNode(); //构造的前缀树的根节点
+
     /**
      * 向前缀树加入一个敏感词, 到达敏感词最后一个字符时, 将该前缀树节点中isEnd = true
      * @param lineTxt 敏感词
@@ -98,11 +100,10 @@ public class SensitiveWordsService implements InitializingBean {
             }
             tempNode = node; //当前节点指向下一节点
             if (i == lineTxt.length() - 1)
-                tempNode.setKeyWotdEnd(true); //该节点为敏感词结尾
+                tempNode.setKeyWordEnd(true); //该节点为敏感词结尾
         }
     }
 
-    private TrieNode root = new TrieNode(); //构造的前缀树的根节点
 
 
 
@@ -110,7 +111,7 @@ public class SensitiveWordsService implements InitializingBean {
      * 利用前缀树对输入文本进行过滤(核心)
      * @param input 输入文本
      * @return 过滤后的文本
-     */
+     *//*
     public String filter(String input) {
         if (StringUtils.isEmpty(input)) {
             return input;
@@ -118,9 +119,9 @@ public class SensitiveWordsService implements InitializingBean {
 
         StringBuilder res = new StringBuilder();
         String replacement = "***"; //敏感词替代
-        TrieNode tempNode = root;
-        int begin = 0;
-        int cur = 0; //当前移动指针
+        TrieNode tempNode = root; //前缀树指针
+        int begin = 0; //指针3
+        int cur = 0; //指针2 - 移动到结尾则标志结束
         while (cur < input.length()) {
             char ch = input.charAt(cur);
 
@@ -156,8 +157,59 @@ public class SensitiveWordsService implements InitializingBean {
         res.append(input.substring(begin));
         return res.toString();
 
-    }
+    }*/
 
+    /**
+     * 利用前缀树对输入文本进行过滤(核心)
+     * @param input 输入文本
+     * @return 过滤后的文本
+     */
+    public String filter(String input) {
+        if (StringUtils.isEmpty(input)) {
+            return input;
+        }
+
+        StringBuilder res = new StringBuilder();
+        String replacement = "***"; //敏感词替代
+        TrieNode tempNode = root; //前缀树指针
+        int begin = 0; //指针2- 移动到结尾则标志结束
+        int cur = 0; //指针3
+        while (begin < input.length()) {
+            char ch = input.charAt(cur);
+
+            //比如这样的也能被过滤, *暴๑乛◡乛๑力*
+            if (isSymbol(ch)) {
+                //非法字符, 不进行过滤
+                if (tempNode == root) {
+                    res.append(ch);
+                    begin++;
+                }
+                cur++;
+                continue;
+            }
+            tempNode = tempNode.getSubNode(ch);
+
+            if (tempNode == null) {
+                //如果前缀树下个节点为空
+                res.append(input.charAt(begin));
+                cur = begin+1;
+                begin = cur;
+                tempNode = root;
+            } else if (tempNode.isKeyWordEnd()) {
+                //前缀树下一个节点不为空, 且找到敏感词
+                res.append(replacement);
+                cur++;
+                begin = cur;
+                tempNode = root;
+            } else {
+                //前缀树下一个节点不为空, 且 不是敏感词, 继续移动指针进行比较
+                cur++;
+            }
+        }
+        res.append(input.substring(begin)); //最后一次没有匹配成功, 添加剩余的字符串
+        return res.toString();
+
+    }
     //判断该字符是不是无效字符(数字,字母,中文汉字以外的均为无效字符)
     private boolean isSymbol(char c) {
         //东亚文字: 0x2E80 - 0x9FFF
@@ -169,7 +221,7 @@ public class SensitiveWordsService implements InitializingBean {
     public static void main(String[] args) {
         SensitiveWordsService sensitiveWordsService = new SensitiveWordsService();
         sensitiveWordsService.addSensitiveWord("暴力");
-        String input = "ji, **暴**力**了";
+        String input = "ji, <暴力>**了";
         System.out.println(sensitiveWordsService.filter(input));
 
     }
